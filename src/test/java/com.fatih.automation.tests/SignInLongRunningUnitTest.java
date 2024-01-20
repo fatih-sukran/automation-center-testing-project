ba9eeac7c5254cfb8692159ea75e70f2d97c2e8f
Fatih Şükran <fth.21.81@gmail.com>
2024-01-20 20:24:32 +0300

    initial commit

---
+++ src/test/java/com.fatih.automation.tests/SignInLongRunningUnitTest.java
@@ -0,0 +1,14 @@
+package com.fatih.automation.tests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+public class SignInLongRunningUnitTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SignInLongRunningUnitTest.class);
+
+    @Test
+    public void whenCalledFromSuite_thanOK() {
+        LOGGER.info("SignIn successful");
+    }
+}
